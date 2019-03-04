555e1ca0ad4bd57aab4f7bee74bf68d4b650b44b
eempc <evc484@gmail.com>
2019-03-04 21:08:44 +0000

    First commit of tutorial, anatomy of HTML pages and 2 classes

---
+++ WebApplication1/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1 {
+    public class Customer {
+        // No constructor required
+        public int Id { get; set; }
+
+        [Required, StringLength(10)]
+        public string Name { get; set; }
+        
+
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore; // Required for the DBcontext
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// Static stuff is in wwww.root (use static files?) like css, images, js
+// Pages is the razor pages
+
+namespace WebApplication1 {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("name")
+            
+            ); //AppDbContext is a class .cs file that needs to be generated (ctrl + .)
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+            // might need to declare default routes?
+        }
+    }
+}
